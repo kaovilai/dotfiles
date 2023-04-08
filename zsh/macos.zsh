8a7116f75dc97abb431b64e29feb9069a04afd6c
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2023-04-08 01:48:54 -0400

    edge alias
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -2,6 +2,8 @@ alias code='open -a Visual\ Studio\ Code'
 alias ghd='open -a GitHub\ Desktop'
 alias finder='open -R'
 alias idlesleeppreventers='pmset -g assertions'
+alias edgedev='open -a Microsoft\ Edge\ Dev'
+alias edge='open -a Microsoft\ Edge'
 
 PATH=$PATH:/Users/tiger/Library/Python/3.9/bin
 
