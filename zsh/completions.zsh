fe92156a4132164e25449dd63ab544fd3c14491a
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2023-06-03 23:48:34 -0400

    add timeout
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -18,14 +18,14 @@ if [ $(command -v gh) ]; then
 fi
 if [ $(command -v docker) ]; then
   # get completion by curling from https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/zsh/_docker
-  curl -s https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/zsh/_docker > ~/_docker || echo "offline - _docker"
+  timeout 1s sh -c "curl -s https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/zsh/_docker > ~/_docker" || echo "offline - _docker"
   source <(cat ~/_docker)
   compdef _docker docker
 fi
 
 if [ $(command -v podman) ]; then
   # https://raw.githubusercontent.com/containers/podman/main/completions/zsh/_podman
-  curl -s https://raw.githubusercontent.com/containers/podman/main/completions/zsh/_podman > ~/_podman || echo "offline - _podman"
+  timeout 1s sh -c "curl -s https://raw.githubusercontent.com/containers/podman/main/completions/zsh/_podman > ~/_podman" || echo "offline - _podman"
   source <(cat ~/_podman)
   compdef _podman podman
 fi
