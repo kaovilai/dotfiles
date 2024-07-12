69aa01c4f738dcac400558dd8211059d67c5cb2d
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-07-12 15:11:23 -0400

    brew completion
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -16,3 +16,5 @@ function podmanMachineReset(){
 function setTTLforHotspot(){
     sudo sysctl -w net.inet.ip.ttl=65
 }
+# https://docs.brew.sh/Shell-Completion says need to be done before compinit which is in znap.zsh sourced right after this in .zshrc
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
