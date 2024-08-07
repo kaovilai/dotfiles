264b842d960d331cf415b347d81a339885e69ad4
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-08-07 13:55:34 -0400

    auto proxy
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -18,3 +18,18 @@ function setTTLforHotspot(){
 }
 # https://docs.brew.sh/Shell-Completion says need to be done before compinit which is in znap.zsh sourced right after this in .zshrc
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+WIFI_NAME=$(networksetup -getairportnetwork en0 | cut -d " " -f 4)
+if [[ "$WIFI_NAME" = "$TF_NETWORK_NAME" ]]; then
+    export TF_ROUTER_IP=$(networksetup -getinfo Wi-Fi | grep -e "^Router" | cut -d " " -f 2)
+    export TF_ROUTER_PROXY_PORT=8228
+    export http_proxy=$TF_ROUTER_IP:$TF_ROUTER_PROXY_PORT
+    export https_proxy=$TF_ROUTER_IP:$TF_ROUTER_PROXY_PORT
+    networksetup -setwebproxy Wi-Fi $TF_ROUTER_IP $TF_ROUTER_PROXY_PORT
+    networksetup -setsecurewebproxy Wi-Fi $TF_ROUTER_IP $TF_ROUTER_PROXY_PORT
+    networksetup -setwebproxystate Wi-Fi on
+    networksetup -setsecurewebproxystate Wi-Fi on
+else
+    networksetup -setwebproxystate Wi-Fi off
+    networksetup -setsecurewebproxystate Wi-Fi off
+fi
