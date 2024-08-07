0f34f9efea09fc0e47b06d89e8529f16325bffab
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-08-07 10:28:53 -0400

    cherrypick branch
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -2,7 +2,17 @@
 # update go mod
 # ORG=openshift REPO=$(basename $PWD) BRANCH=oadp-1.2 PACKAGE=google.golang.org/grpc@v1.56.3 ISSUE="CVE-2023-44487-gRPC-Go" && git checkout $ORG/$BRANCH && git checkout -b $ISSUE-$BRANCH && go get $package && go mod tidy && git add go.mod go.sum && git commit -m "$BRANCH: CVE-2023-44487 gRPC-Go HTTP/2 Rapid Reset vulnerability" --signoff && gh pr create --base $BRANCH --repo $ORG/velero-plugin-for-gcp
 
-# Usage: cherrypick-pr <#PR-number>
+# Usage: cherrypick-pr <#PR-number> ...
 function cherrypick-pr() {
     echo $* | xargs -n 1 -I {} sh -c 'git cherry-pick $(gh pr view {} --json commits | jq ".commits[].oid" --raw-output | xargs)'
+}
+
+# Usage: cherrypick-pr-to-branch <#PR-number> <remote/branch> <new-branch-name>
+function cherrypick-pr-to-branch() {
+    local PR_NUMBER=$1
+    local BRANCH=$2
+    local NEW_BRANCH=$3
+    echo "Cherry-picking PR $PR_NUMBER to branch $BRANCH"
+    git checkout -b $NEW_BRANCH $BRANCH || (git checkout $NEW_BRANCH && git reset --hard $BRANCH)
+    cherrypick-pr $PR_NUMBER
 }
