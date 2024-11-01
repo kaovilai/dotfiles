b4d350b21939c5c0f939d29a9b6362dd636fbe23
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-11-01 08:00:58 -0500

    feat(zsh/go.zsh): add aliases for formatting changed Go files in main and master branches
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,1 +1,3 @@
 alias gofmtgitdiff='git diff --name-only  | xargs -L 1 go fmt'
+alias gofmtgitdiffmain='git diff upstream/main --name-only | grep .go$  | xargs -L 1 go fmt'
+alias gofmtgitdiffmaster='git diff upstream/master --name-only | grep .go$  | xargs -L 1 go fmt'
