df8c7817fc8116e3a7a4a9a2f626158c2a713200
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-11-02 08:35:03 -0500

    fix(zsh/macos.zsh): add functions to set and unset HTTP proxy for TF network, and set SOCKS proxy for S23 network feat(zsh/macos.zsh): extract logic to set and unset proxies into separate functions for better organization and reusability
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -21,11 +21,7 @@ function setTTLforHotspot(){
 # https://docs.brew.sh/Shell-Completion says need to be done before compinit which is in znap.zsh sourced right after this in .zshrc
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
-# To get git to work over ssh via 443 proxy,
-# replace .git/config `git@github.com:(.*)/`
-# with `ssh://git@ssh.github.com:443/$1/`
-WIFI_NAME=$(networksetup -getairportnetwork en0 | cut -d " " -f 4)
-if [[ "$WIFI_NAME" = "$TF_NETWORK_NAME" ]]; then
+function setTFproxy(){
     export TF_ROUTER_IP=$(networksetup -getinfo Wi-Fi | grep -e "^Router" | cut -d " " -f 2)
     export TF_ROUTER_PROXY_PORT=8228
     export http_proxy=$TF_ROUTER_IP:$TF_ROUTER_PROXY_PORT
@@ -34,6 +30,34 @@ if [[ "$WIFI_NAME" = "$TF_NETWORK_NAME" ]]; then
     networksetup -setsecurewebproxy Wi-Fi $TF_ROUTER_IP $TF_ROUTER_PROXY_PORT
     networksetup -setwebproxystate Wi-Fi on
     networksetup -setsecurewebproxystate Wi-Fi on
+}
+
+function unsetTFproxy(){
+    networksetup -setwebproxystate Wi-Fi off
+    networksetup -setsecurewebproxystate Wi-Fi off
+    unset http_proxy
+    unset https_proxy
+}
+
+function setSOCKSproxy(){
+    export SOCKS_ROUTER_IP=$(networksetup -getinfo Wi-Fi | grep -e "^Router" | cut -d " " -f 2)
+    export SOCKS_ROUTER_PROXY_PORT=1888
+    networksetup -setsocksfirewallproxy Wi-Fi $SOCKS_ROUTER_IP $SOCKS_ROUTER_PROXY_PORT off
+    networksetup -setsocksfirewallproxystate Wi-Fi on
+}
+
+function unsetSOCKSproxy(){
+    networksetup -setsocksfirewallproxystate Wi-Fi off
+}
+if [[ "$WIFI_NAME" = "S23" ]]; then
+    (curl --silent --socks5 $SOCKS_ROUTER_IP:$SOCKS_ROUTER_PROXY_PORT http://www.google.com && setSOCKSproxy) || unsetSOCKSproxy
+fi
+# To get git to work over ssh via 443 proxy,
+# replace .git/config `git@github.com:(.*)/`
+# with `ssh://git@ssh.github.com:443/$1/`
+WIFI_NAME=$(networksetup -getairportnetwork en0 | cut -d " " -f 4)
+if [[ "$WIFI_NAME" = "$TF_NETWORK_NAME" ]]; then
+    setTFproxy
     # mkdir -p ~/.ssh/tigerdotfiles/
     # echo "Host github.com
     # Hostname github.com
@@ -41,9 +65,5 @@ if [[ "$WIFI_NAME" = "$TF_NETWORK_NAME" ]]; then
     # ForwardAgent yes
     # ProxyCommand $(which socat) - PROXY:$TF_ROUTER_IP:%h:%p,proxyport=$TF_ROUTER_PROXY_PORT" > ~/.ssh/tigerdotfiles/config
 else
-    networksetup -setwebproxystate Wi-Fi off
-    networksetup -setsecurewebproxystate Wi-Fi off
-    unset http_proxy
-    unset https_proxy
-    echo "" > ~/.ssh/tigerdotfiles/config
+    unsetTFproxy
 fi
