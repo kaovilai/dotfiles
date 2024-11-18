1224a342e2827f5293bcd57f24962aeae459667f
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-11-18 08:37:59 -0500

    fix(podman.zsh): remove redundant docker host environment setup to improve performance
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/podman.zsh
+++ zsh/podman.zsh
@@ -2,4 +2,5 @@ znap function updateDockerHostEnv() {
   command -v podman && DOCKER_HOST=$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
 }
 
-command -v podman && DOCKER_HOST=$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
+# not needed when eval $(crc podman-env)
+# command -v podman && DOCKER_HOST=$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
