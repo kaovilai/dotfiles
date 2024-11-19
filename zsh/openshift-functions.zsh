ec7f368525797bbde115c67756e50ef511b39fa8
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-11-19 00:34:57 -0500

    fix(zsh/openshift-functions.zsh): replace hardcoded kubeconfig path with KUBECONFIG export to improve portability and security
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/openshift-functions.zsh
+++ zsh/openshift-functions.zsh
@@ -1,5 +1,5 @@
 alias kubectl=oc
-alias oc-login-crc='oc login --server=https://api.crc.testing:6443 -u kubeadmin -p $(cat ~/.crc/machines/crc/kubeadmin-password)'
+alias oc-login-crc='export KUBECONFIG=~/.crc/machines/crc/kubeconfig'
 alias oc-registry-login='oc registry login'
 alias oc-registry-route='oc get route -n openshift-image-registry default-route -o jsonpath={.spec.host}'
 alias crc-kubeadminpass='cat ~/.crc/machines/crc/kubeadmin-password'
@@ -160,4 +160,127 @@ znap function crc-start-version(){
         (cat ~/Downloads/$1-crc.pkg >/dev/null || curl https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/$1/crc-macos-installer.pkg -L -o ~/Downloads/$1-crc.pkg) && sudo installer -pkg ~/Downloads/$1-crc.pkg -target LocalSystem
     fi
     crc version && crc setup && crc start --log-level debug && crc status --log-level debug
+}
+
+export OPENSHIFT_REJECT_VERSIONS_EXPRESSION="nightly|rc|fc|ci|ec"
+function openshift-patch-versions-arm64(){
+    curl --silent https://openshift-release-artifacts-arm64.apps.ci.l2s4.p1.openshiftapps.com/ | grep -vE $OPENSHIFT_REJECT_VERSIONS_EXPRESSION | cut -d '"' -f 2 | sed "s/\///g" | grep -vE "<|>|en|utf|^$" | grep -ve "\.\." | sort -V | awk -F. '{if(!a[$1"."$2]++)print $1"."$2"."$NF}'
+}
+function openshift-patch-versions-amd64(){
+    curl --silent https://openshift-release-artifacts.apps.ci.l2s4.p1.openshiftapps.com/ | grep -vE $OPENSHIFT_REJECT_VERSIONS_EXPRESSION | cut -d '"' -f 2 | sed "s/\///g" | grep -vE "<|>|en|utf|^$" | grep -ve "\.\." | sort -V | awk -F. '{if(!a[$1"."$2]++)print $1"."$2"."$NF}'
+}
+alias latest-openshift-patch-version-arm64="openshift-patch-versions-arm64 | tail -n 1"
+alias latest-openshift-patch-version-amd64="openshift-patch-versions-amd64 | tail -n 1"
+alias latest-openshift-minor-version-arm64="openshift-patch-versions-arm64 | tail -n 1 | cut -d '.' -f 1,2"
+
+if [ -n "$(command -v sw_vers)" ]; then
+    ocpclientos='mac'
+    ocpclientarch=$(arch)
+else
+    if [ -n $(command -v lsb_release) ]; then
+        ocpclientos='linux'
+        ocpclientarch=$(dpkg --print-architecture)
+    else
+        echo "openshift-functions.zsh: Unknown OS"
+    fi
+fi
+
+function install-oc(){
+    curl --silent https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-$ocpclientos-$ocpclientarch.tar.gz -o ~/Downloads/openshift-client-$ocpclientos-$ocpclientarch.tar.gz && \
+    tar -xvf ~/Downloads/openshift-client-$ocpclientos-$ocpclientarch.tar.gz -C ~/Downloads && \
+    sudo mv ~/Downloads/oc /usr/local/bin && \
+    sudo mv ~/Downloads/kubectl /usr/local/bin && \
+    rm ~/Downloads/openshift-client-$ocpclientos-$ocpclientarch.tar.gz
+    rm ~/Downloads/README.md
+}
+
+function install-ocp-installer(){
+    curl --silent https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-$ocpclientos-$ocpclientarch.tar.gz -o ~/Downloads/openshift-install-$ocpclientos-$ocpclientarch.tar.gz && \
+    tar -xvf ~/Downloads/openshift-install-$ocpclientos-$ocpclientarch.tar.gz -C ~/Downloads && \
+    sudo mv ~/Downloads/openshift-install /usr/local/bin
+    rm ~/Downloads/openshift-install-$ocpclientos-$ocpclientarch.tar.gz
+    rm ~/Downloads/README.md
+}
+
+function install-ccoctl(){
+    go install github.com/openshift/cloud-credential-operator/cmd/ccoctl@release-$(latest-openshift-minor-version-arm64)
+}
+
+function install-opm(){
+    go install github.com/operator-framework/operator-registry/cmd/opm@latest
+}
+# directory containing the manifests for many clusters
+OCP_MANIFESTS_DIR=~/OCP/manifests
+TODAY=$(date +%Y%m%d)
+# create a cluster with gcp workload identity using CCO manual mode
+function create-ocp-gcp-wif(){
+    # openshift-install create install-config --dir $OCP_MANIFESTS_DIR/$TODAY-gcp-wif --log-level debug
+    # https://docs.redhat.com/en/documentation/openshift_container_platform/4.17/html-single/installing_on_gcp/index#cco-ccoctl-configuring_installing-gcp-customizations
+    # prompt and remove if exists already so user can interrupt if uninstall is needed.
+    OCP_CREATE_DIR=$OCP_MANIFESTS_DIR/$TODAY-gcp-wif
+    CLUSTER_NAME=tkaovila-$TODAY-wif #max 21 char allowed
+    openshift-install destroy cluster --dir $OCP_CREATE_DIR || echo "no existing cluster"
+    openshift-install destroy bootstrap --dir $OCP_CREATE_DIR || echo "no existing bootstrap"
+    (ccoctl gcp delete \
+    --name $CLUSTER_NAME \
+    --project $GCP_PROJECT_ID \
+    --credentials-requests-dir $OCP_CREATE_DIR/credentials-requests && echo "cleaned up ccoctl gcp resources") || true
+    ((rm -r $OCP_CREATE_DIR && echo "removed existing create dir") || (true && echo "no existing install dir")) && \
+    mkdir -p $OCP_CREATE_DIR && \
+    echo "additionalTrustBundlePolicy: Proxyonly
+apiVersion: v1
+baseDomain: $GCP_BASEDOMAIN
+compute:
+- architecture: amd64
+  hyperthreading: Enabled
+  name: worker
+  platform: {}
+  replicas: 3
+controlPlane:
+  architecture: amd64
+  hyperthreading: Enabled
+  name: master
+  platform: {}
+  replicas: 3
+metadata:
+  creationTimestamp: null
+  name: $CLUSTER_NAME
+networking:
+  clusterNetwork:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  machineNetwork:
+  - cidr: 10.0.0.0/16
+  networkType: OVNKubernetes
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  gcp:
+    projectID: $GCP_PROJECT_ID
+    region: $GCP_REGION
+publish: External
+credentialsMode: Manual # needed for WIF at the time of prior writing at https://github.com/openshift/oadp-operator/wiki/GCP-WIF-Authentication-on-OpenShift
+pullSecret: '$(cat ~/pull-secret.txt)'
+sshKey: |
+  $(cat ~/.ssh/id_rsa.pub)
+" > $OCP_CREATE_DIR/install-config.yaml && echo "created install-config.yaml" || return 1
+# Set a $RELEASE_IMAGE variable with the release image from your installation file by running the following command:
+RELEASE_IMAGE=$(openshift-install version | awk '/release image/ {print $3}')
+# Extract the list of CredentialsRequest custom resources (CRs) from the OpenShift Container Platform release image by running the following command:
+echo "extracting credential-requests" && oc adm release extract \
+  --from=$RELEASE_IMAGE \
+  --credentials-requests \
+  --included \
+  --install-config=$OCP_CREATE_DIR/install-config.yaml \
+  --to=$OCP_CREATE_DIR/credentials-requests || return 1 #credential requests are stored in credentials-requests dir
+ccoctl gcp create-all \
+--name $CLUSTER_NAME \
+--project $GCP_PROJECT_ID \
+--region $GCP_REGION \
+--output-dir $OCP_CREATE_DIR \
+--credentials-requests-dir $OCP_CREATE_DIR/credentials-requests || return 1
+openshift-install create manifests --dir $OCP_CREATE_DIR || return 1
+cp $OCP_CREATE_DIR/credentials-requests/* $OCP_CREATE_DIR/manifests/ || return 1 # copy cred requests to manifests dir, ccoctl delete will delete cred requests in separate dir
+openshift-install create cluster --dir $OCP_CREATE_DIR \
+    --log-level=info
 }
