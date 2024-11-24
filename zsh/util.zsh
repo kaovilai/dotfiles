61bb10e1dd86674f99498cce3e1a0f4a78785b1b
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2024-11-24 03:53:39 -0500

    fix(zsh/util.zsh): remove znap prefix from function declarations to follow naming conventions feat(zsh/util.zsh): add new-changelog function to generate changelog for velero repos based on GH_PR_NUMBER and CHANGELOG_BODY variables
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -18,7 +18,7 @@ znap function cherrypick-pr-to-branch() {
 }
 
 # Helper function to create a new changelog for velero repos
-znap function new-changelog(){
+function new-changelog(){
     GH_LOGIN=$(gh pr view --json author --jq .author.login 2> /dev/null)
     GH_PR_NUMBER=$(gh pr view --json number --jq .number 2> /dev/null)
     CHANGELOG_BODY="$(gh pr view --json title --jq .title)"
