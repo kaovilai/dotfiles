bb97e1c83177ad1c8a78b92acbdaf9d2c1c8fc67
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2025-03-05 18:16:11 -0600

    fix(zsh/util.zsh): add newline characters for improved readability and shell scripting best practices
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -64,7 +64,13 @@ function go-mod-upgrade(){
 # Examples: GOTOOLCHAIN=go1.23.6 go-mod-upgrade-dirs "velero*" golang.org/x/oauth2@v0.27.0
 # Examples: GOTOOLCHAIN=go1.23.6 go-mod-upgrade-dirs "velero*" golang.org/x/crypto@v0.35.0 "gsed -i \"s/golang:1.22-bookworm/golang:1.23-bookworm/g\" Dockerfile && git add Dockerfile" CVE-2025-22869
 function go-mod-upgrade-dirs(){
-    find . -type d -maxdepth 1 -name "$1" -exec sh -c "cd {} && pwd && git fetch upstream && (git checkout upstream/main || git checkout upstream/master) && (git checkout -b $2 || git checkout $2) && go get $2 && go mod tidy && git add go.mod go.sum && sh -c \"$3\" && git commit -sm \"$4$2\" && gh pr create --web --title \"$4$2\"" \;
+    find . -type d -maxdepth 1 -name "$1" -exec sh -c "cd {} && pwd && \
+        git fetch upstream && (git checkout upstream/main || git checkout upstream/master) && \
+        (git checkout -b $2 || git checkout $2) && \
+        go get $2 && go mod tidy && git add go.mod go.sum && \
+        sh -c \"$3\" && \
+        git commit -sm \"$4$2\" && \
+        gh pr create --web --title \"$4$2\"" \;
 }
 
 # execute commands in dirs matched by find . -type d -maxdepth 1 -name "<$1>"
