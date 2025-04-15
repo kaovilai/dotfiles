609f1ffd1373cf92aa5069150b2f8e310c0affdb
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2025-04-15 18:21:34 -0400

    chore(zsh/aliases): update ollama aliases and add new aliases feat(zsh/aliases): add new aliases for ollama and kind
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- zsh/aliases/misc.zsh
+++ zsh/aliases/misc.zsh
@@ -1,9 +1,9 @@
 # Miscellaneous aliases
 alias occonsole='edge $(oc whoami --show-console)'
 alias eks-create='aws eks create-cluster --name tkaovila-eks --role-arn=$(aws iam get-role --role-name tkaovila-eks --output yaml | yq .Role.Arn) --resources-vpc-config subnetIds=$(aws ec2 describe-subnets --output yaml | yq -r ".Subnets[] | select(.Tags[] | .Value == \"tkaovila-eks-subnet\") | .SubnetId"),$(aws ec2 describe-subnets --output yaml | yq -r ".Subnets[] | select(.Tags[] | .Value == \"tkaovila-eks-subnet2\") | .SubnetId")'
-alias ollama-run='ollama run llama3.1:latest'
+alias ollama-run='ollama run llama3.2:3b'
 alias ollama-run-deepseek='ollama run deepseek-r1:8b'
-alias ollama-pull='ollama pull llama3-gradient:latest && llama3.1:latest'
+alias ollama-pull='ollama pull llama3-gradient:latest && llama3.2:3b'
 alias source-zshrc='source ~/.zshrc'
 alias listening-ports='lsof -i -P | grep LISTEN'
 alias go-install-kind='go install sigs.k8s.io/kind@latest'
